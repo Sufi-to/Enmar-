617671ea81a3e853462216f5c98e522f63c3aad1
Sufi-to <3thomas225@gmail.com>
2024-06-24 19:02:58 +0400

    fixed enrollments and Course Admin

--- ELP/learningPlatform/forms.py
+++ ELP/learningPlatform/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, Course, Enrollment
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 from .models import Profile
@@ -12,3 +12,23 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'is_learner', 'is_instructor', 'bio']
+
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['instructor'].queryset = User.objects.filter(is_instructor=True)
+        
+
+class EnrollmentForm(forms.ModelForm):
+    class Meta:
+        model = Enrollment
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['learner'].queryset = User.objects.filter(is_learner=True)
